ba78cc9a268180b5416780d1c81275551e36a343
Anna_Gorova <koshka20.03.19@gmail.com>
2025-10-03 23:33:11 +0300

    Виправлено помилку в назві проекту: Restiran → Restoran

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfAppRestiranOrder
+namespace WpfAppRestoranOrder
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
